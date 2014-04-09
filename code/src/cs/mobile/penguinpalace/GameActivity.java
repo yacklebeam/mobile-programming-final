44b9a5901d455a1054e10d4ae39d245f6b3280c7
Jacob Troxel <jaketroxel@gmail.com>
2014-04-09 01:00:20 -0700

    Still debugging

--- code/src/cs/mobile/penguinpalace/GameActivity.java
+++ code/src/cs/mobile/penguinpalace/GameActivity.java
@@ -9,6 +9,5 @@ public class GameActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
-		// do some game things.
 	}
 }
