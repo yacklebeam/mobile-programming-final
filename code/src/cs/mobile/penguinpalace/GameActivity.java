acac213d22c81372091a9f9983a182ed5c4ef17d
Jacob Troxel <jaketroxel@gmail.com>
2014-04-09 00:41:23 -0700

    Added more to title screen.  Added title background, and mock game activity.

---
+++ code/src/cs/mobile/penguinpalace/GameActivity.java
@@ -0,0 +1,13 @@
+package cs.mobile.penguinpalace;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game);
+	}
+}
