22a27f4dcad11e979e3e04444575e93b6f5c8119
Jacob Troxel <jaketroxel@gmail.com>
2014-04-09 00:51:57 -0700

    Attempting to fix some bs with eclipse. Last commit was flagged.

--- code/src/cs/mobile/penguinpalace/GameActivity.java
+++ code/src/cs/mobile/penguinpalace/GameActivity.java
@@ -9,5 +9,6 @@ public class GameActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
+		// do some game things.
 	}
 }
